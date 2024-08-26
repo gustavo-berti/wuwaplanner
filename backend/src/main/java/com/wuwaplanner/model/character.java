2b0096eba6066c0e8c44ef51cff923ddf0c203c7
Gustavo <gutigimenez15@gmail.com>
2024-08-26 14:54:52 -0300

    Alterações Model

--- backend/src/main/java/com/wuwaplanner/model/character.java
+++ backend/src/main/java/com/wuwaplanner/model/character.java
@@ -5,15 +5,108 @@ import java.util.List;
 
 public class Character {
    private String name;
-   private int tier;
-   private Boss boss;
-   private WeaklyBoss weaklyBoss;
-   private Flower flower;
-   private List<MaterialFarm> materialFarm;
-   private List<MaterialWorld> materialWorld;
+   private String tier;
+   private String boss;
+   private String weaklyBoss;
+   private String flower;
+   private String materialFarm;
+   private String materialWorld;
    private int currentLevel;
    private int targetLevel;
    private List<Skill> skills;
    private Map<String,Boolean> passives;
+
+   public Character(String name, String tier) {
+      this.name = name;
+      this.tier = tier;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getTier() {
+      return tier;
+   }
+
+   public void setTier(String tier) {
+      this.tier = tier;
+   }
+
+   public String getBoss() {
+      return boss;
+   }
+
+   public void setBoss(String boss) {
+      this.boss = boss;
+   }
+
+   public String getWeaklyBoss() {
+      return weaklyBoss;
+   }
+
+   public void setWeaklyBoss(String weaklyBoss) {
+      this.weaklyBoss = weaklyBoss;
+   }
+
+   public String getFlower() {
+      return flower;
+   }
+
+   public void setFlower(String flower) {
+      this.flower = flower;
+   }
+
+   public String getMaterialFarm() {
+      return materialFarm;
+   }
+
+   public void setMaterialFarm(String materialFarm) {
+      this.materialFarm = materialFarm;
+   }
+
+   public String getMaterialWorld() {
+      return materialWorld;
+   }
+
+   public void setMaterialWorld(String materialWorld) {
+      this.materialWorld = materialWorld;
+   }
+
+   public int getCurrentLevel() {
+      return currentLevel;
+   }
+
+   public void setCurrentLevel(int currentLevel) {
+      this.currentLevel = currentLevel;
+   }
+
+   public int getTargetLevel() {
+      return targetLevel;
+   }
+
+   public void setTargetLevel(int targetLevel) {
+      this.targetLevel = targetLevel;
+   }
+
+   public List<Skill> getSkills() {
+      return skills;
+   }
+
+   public void setSkills(List<Skill> skills) {
+      this.skills = skills;
+   }
+
+   public Map<String, Boolean> getPassives() {
+      return passives;
+   }
+
+   public void setPassives(Map<String, Boolean> passives) {
+      this.passives = passives;
+   }
 
 }
--- backend/src/main/java/com/wuwaplanner/model/weapon.java
+++ backend/src/main/java/com/wuwaplanner/model/weapon.java
@@ -2,10 +2,63 @@ package com.wuwaplanner.model;
 
 public class Weapon {
     private String name;
-    private int tier;
-    private MaterialFarm materialFarm;
-    private MaterialWorld materialWorld;
+    private String tier;
+    private String materialFarm;
+    private String materialWorld;
     private int currentLevel;
     private int targetLevel;
+
+    public Weapon(String name, String tier) {
+        this.name = name;
+        this.tier = tier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTier() {
+        return tier;
+    }
+
+    public void setTier(String tier) {
+        this.tier = tier;
+    }
+
+    public String getMaterialFarm() {
+        return materialFarm;
+    }
+
+    public void setMaterialFarm(String materialFarm) {
+        this.materialFarm = materialFarm;
+    }
+
+    public String getMaterialWorld() {
+        return materialWorld;
+    }
+
+    public void setMaterialWorld(String materialWorld) {
+        this.materialWorld = materialWorld;
+    }
+
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void setCurrentLevel(int currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    public int getTargetLevel() {
+        return targetLevel;
+    }
+
+    public void setTargetLevel(int targetLevel) {
+        this.targetLevel = targetLevel;
+    }
     
 }
